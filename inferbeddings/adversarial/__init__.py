16cc2e63da47e78840f6f3f93fb4b0b998969c54
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-01-16 15:43:43 +0100

    exposing GroundLoss from the inferbeddings.adversarial package

--- inferbeddings/adversarial/__init__.py
+++ inferbeddings/adversarial/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from inferbeddings.adversarial.base import Adversarial
+from inferbeddings.adversarial.ground import GroundLoss
 
-__all__ = ['Adversarial']
+__all__ = ['Adversarial',
+           'GroundLoss']
