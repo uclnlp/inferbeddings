16505b88d864501f3c084ae8ac3be196e70e195d
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-01-17 13:09:12 +0100

    update __init__.py

--- inferbeddings/adversarial/__init__.py
+++ inferbeddings/adversarial/__init__.py
@@ -2,8 +2,6 @@
 
 from inferbeddings.adversarial.base import Adversarial
 from inferbeddings.adversarial.ground import GroundLoss
-from inferbeddings.adversarial.util import sample_mappings
 
 __all__ = ['Adversarial',
-           'GroundLoss',
-           'sample_mappings']
+           'GroundLoss']
