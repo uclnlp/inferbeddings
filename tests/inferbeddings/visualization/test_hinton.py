bd16b1ca530a152879f40ab4cb424607ea7b7a0e
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-01-25 16:15:30 +0100

    update tests for hinton diagrams

--- tests/inferbeddings/visualization/test_hinton.py
+++ tests/inferbeddings/visualization/test_hinton.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from inferbeddings.visualization.hinton import HintonDiagram
+from inferbeddings.visualization import hinton_diagram
 
 
 def test_hinton_diagram():
     data = np.random.randn(10, 50)
-    hd = HintonDiagram(is_terminal=False)
-    diagram = hd(data)
+    diagram = hinton_diagram(data)
     assert len(diagram) == 6967
