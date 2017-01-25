96dba7434c37b07d2281319ee2ba3c999047dc45
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-01-25 16:20:05 +0100

    better variable names

--- tests/inferbeddings/model/test_models.py
+++ tests/inferbeddings/model/test_models.py
@@ -29,11 +29,11 @@ def test_translating_embeddings_score():
     with tf.Session() as session:
         session.run(init_op)
 
-        sscores = session.run(scores)
-        assert(sscores.shape[0] == batch_size)
+        scores_value = session.run(scores)
+        assert(scores_value.shape[0] == batch_size)
 
         tmp = - np.sum(np.abs(E[:, 0, :] + R[:, 0, :] - E[:, 1, :]), axis=1)
-        assert(np.isclose(sscores, tmp).all())
+        assert(np.isclose(scores_value, tmp).all())
 
 
 def test_bilinear_diagonal_score():
@@ -56,11 +56,11 @@ def test_bilinear_diagonal_score():
     with tf.Session() as session:
         session.run(init_op)
 
-        sscores = session.run(scores)
-        assert(sscores.shape[0] == batch_size)
+        scores_value = session.run(scores)
+        assert(scores_value.shape[0] == batch_size)
 
         tmp = - np.sum(np.abs(E[:, 0, :] * R[:, 0, :] - E[:, 1, :]), axis=1)
-        assert(np.isclose(sscores, tmp).all())
+        assert(np.isclose(scores_value, tmp).all())
 
 
 def test_bilinear_score():
@@ -84,9 +84,9 @@ def test_bilinear_score():
     with tf.Session() as session:
         session.run(init_op)
 
-        sscores = session.run(scores)
+        scores_value = session.run(scores)
 
-        assert (sscores.shape[0] == batch_size)
+        assert (scores_value.shape[0] == batch_size)
 
         for i in range(batch_size):
             es, eo = E[i, 0, :], E[i, 1, :]
@@ -96,7 +96,7 @@ def test_bilinear_score():
             ep4 = np.reshape(R[i, 3, :], (entity_embedding_size, entity_embedding_size))
             ep = np.dot(np.dot(np.dot(ep1, ep2), ep3), ep4)
 
-            np.testing.assert_allclose(sscores[i], np.dot(np.dot(es, ep), eo))
+            np.testing.assert_allclose(scores_value[i], np.dot(np.dot(es, ep), eo))
 
 if __name__ == '__main__':
     pytest.main([__file__])
