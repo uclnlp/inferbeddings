853374c5eba46c6b30ab35ff11b2f90b0295b65e
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-01-25 16:32:40 +0100

    added pytest to requirements in setup.py; started writing tests for the adversarial part of the codebase

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@
 from setuptools import setup
 from setuptools import find_packages
 
+requirements = [
+      'tensorflow>=0.8',
+      'parsimonious>=0.7',
+      'terminaltables>=2.0',
+      'colorclass>=2.0']
+
 setup(name='inferbeddings',
       version='0.1.0',
       description='Rule Injection in Knowledge Graph Embeddings via Adversarial Training',
@@ -11,10 +17,7 @@ setup(name='inferbeddings',
       url='https://github.com/uclmr/inferbeddings',
       test_suite='tests',
       license='MIT',
-      install_requires=[
-            'tensorflow>=0.8',
-            'parsimonious>=0.7',
-            'terminaltables>=2.0',
-            'colorclass>=2.0'
-      ],
+      install_requires=requirements,
+      setup_requires=['pytest-runner'] + requirements,
+      tests_require=['pytest'] + requirements,
       packages=find_packages())
