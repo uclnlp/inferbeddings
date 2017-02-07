ba5b1936f4d391ede53b6efaebeb233aac127b10
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-02-07 19:22:18 +0100

    FB15k - 12G -> 10G memory

--- scripts/fb15k/UCL_FB15K_adv_v1.2.py
+++ scripts/fb15k/UCL_FB15K_adv_v1.2.py
@@ -100,7 +100,7 @@ def main(argv):
                 alias = ''
                 job_script = '#$ -S /bin/bash\n' \
                              '#$ -wd /tmp/\n' \
-                             '#$ -l h_vmem=12G,tmem=12G\n' \
+                             '#$ -l h_vmem=10G,tmem=10G\n' \
                              '#$ -l h_rt=96:00:00\n' \
                              '{}\n{}\n'.format(alias, line)
 
