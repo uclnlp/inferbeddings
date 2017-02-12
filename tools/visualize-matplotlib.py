1868d9699ef414766c3a1b3b1130824d1981ff12
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-02-12 21:24:16 +0100

    scoring functions to use for finding maximum violators

--- tools/visualize-matplotlib.py
+++ tools/visualize-matplotlib.py
@@ -17,7 +17,26 @@ logger = logging.getLogger(os.path.basename(sys.argv[0]))
 
 def score_TransE_L1(subject_embedding, predicate_embedding, object_embedding):
     translated_embedding = subject_embedding + predicate_embedding
-    return - np.abs(translated_embedding - object_embedding).sum()
+    return - np.sum(np.abs(translated_embedding - object_embedding))
+
+
+def score_TransE_L2(subject_embedding, predicate_embedding, object_embedding):
+    translated_embedding = subject_embedding + predicate_embedding
+    return - np.sqrt(np.sum(np.square(translated_embedding) - object_embedding))
+
+
+def score_DistMult(subject_embedding, predicate_embedding, object_embedding):
+    return np.sum(subject_embedding * predicate_embedding * object_embedding)
+
+
+def score_ComplEx(subject_embedding, predicate_embedding, object_embedding):
+    def dot3(a, b, c):
+        return np.sum(a * b * c)
+    n = subject_embedding.shape[0]
+    s_re, s_im = subject_embedding[:n // 2], subject_embedding[n // 2:]
+    p_re, p_im = predicate_embedding[:n // 2], predicate_embedding[n // 2:]
+    o_re, o_im = object_embedding[:n // 2], object_embedding[n // 2:]
+    return dot3(s_re, p_re, o_re) + dot3(s_re, p_im, o_im) + dot3(s_im, p_re, o_im) - dot3(s_im, p_im, o_re)
 
 
 def main(argv):
