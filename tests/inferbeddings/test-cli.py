92bb0300f73e4debd86deee8236a3bc56486000f
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-02-15 14:22:25 +0100

    also checking results for TransE

--- tests/inferbeddings/test-cli.py
+++ tests/inferbeddings/test-cli.py
@@ -22,5 +22,21 @@ def test_nations_cli():
     # Hits@10 should be at least 90% even after a limited number of epochs
     assert float(err.split()[-1][:-1]) > 90.0
 
+    cmd = ['./bin/adv-cli.py',
+           '--train', 'data/nations/stratified_folds/0/nations_train.tsv.gz',
+           '--valid', 'data/nations/stratified_folds/0/nations_valid.tsv.gz',
+           '--test', 'data/nations/stratified_folds/0/nations_test.tsv.gz',
+           '--lr', '0.1',
+           '--model', 'TransE',
+           '--similarity', 'l1',
+           '--margin', '1',
+           '--embedding-size', '20',
+           '--nb-epochs', '50']  # 1000
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+
+    # Hits@10 should be at least 90% even after a limited number of epochs
+    assert float(err.split()[-1][:-1]) > 90.0
+
 if __name__ == '__main__':
     pytest.main([__file__])
