3763d7c1015b4bda9366b1faac921dc0fce2d1ce
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-02-15 14:33:19 +0100

    also adding a sanity check for DistMult

--- tests/inferbeddings/test-cli.py
+++ tests/inferbeddings/test-cli.py
@@ -38,5 +38,21 @@ def test_nations_cli():
     # Hits@10 should be at least 90% even after a limited number of epochs
     assert float(err.split()[-1][:-1]) > 90.0
 
+    cmd = ['./bin/adv-cli.py',
+           '--train', 'data/nations/stratified_folds/0/nations_train.tsv.gz',
+           '--valid', 'data/nations/stratified_folds/0/nations_valid.tsv.gz',
+           '--test', 'data/nations/stratified_folds/0/nations_test.tsv.gz',
+           '--lr', '0.1',
+           '--model', 'DistMult',
+           '--similarity', 'dot',
+           '--margin', '1',
+           '--embedding-size', '50',
+           '--nb-epochs', '50']  # 1000
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+
+    # Hits@10 should be at least 85% even after a limited number of epochs
+    assert float(err.split()[-1][:-1]) > 85.0
+
 if __name__ == '__main__':
     pytest.main([__file__])
