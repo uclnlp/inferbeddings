2a3e227d597fa34cb87612f8a58d1dad668b1b81
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-02-16 13:06:21 +0100

    load_glove in __init__.py

--- inferbeddings/io/__init__.py
+++ inferbeddings/io/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from inferbeddings.io.base import iopen, read_triples, save
+from inferbeddings.io.embeddings import load_glove
 
-__all__ = ['iopen', 'read_triples', 'save']
+__all__ = ['iopen',
+           'read_triples',
+           'save',
+           'load_glove']
