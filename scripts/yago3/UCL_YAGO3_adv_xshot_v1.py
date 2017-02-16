c14cbd0ae2296a53a43bee881916afb56019c65d
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-02-16 17:31:50 +0100

    YAGO3 - increasing the maximum runtime

--- scripts/yago3/UCL_YAGO3_adv_xshot_v1.py
+++ scripts/yago3/UCL_YAGO3_adv_xshot_v1.py
@@ -129,7 +129,7 @@ def main(argv):
                 job_script = '#$ -S /bin/bash\n' \
                              '#$ -wd /tmp/\n' \
                              '#$ -l h_vmem=4G,tmem=4G\n' \
-                             '#$ -l h_rt=12:00:00\n' \
+                             '#$ -l h_rt=24:00:00\n' \
                              '{}\n{}\n'.format(alias, line)
 
                 with open(file_name, 'w') as f:
