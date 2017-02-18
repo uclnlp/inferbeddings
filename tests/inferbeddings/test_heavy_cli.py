deb455fc12b49e016718891ca2c6e7cc39158142
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-02-18 20:29:09 +0100

    also adding a test case for TransE

--- tests/inferbeddings/test_heavy_cli.py
+++ tests/inferbeddings/test_heavy_cli.py
@@ -4,7 +4,7 @@ import pytest
 import subprocess
 
 
-def test_wordnet_cli():
+def test_wordnet_complex_cli():
     # Checking if results are still nice
     cmd = ['./bin/adv-cli.py',
            '--train', 'data/wn18/wordnet-mlj12-train.txt',
@@ -20,6 +20,25 @@ def test_wordnet_cli():
 
     # Hits@10 should be at least 90% even after a limited number of epochs
     assert float(err.split()[-1][:-1]) > 93.0
+
+
+def test_wordnet_translating_cli():
+    # Checking if results are still nice
+    cmd = ['./bin/adv-cli.py',
+           '--train', 'data/wn18/wordnet-mlj12-train.txt',
+           '--test', 'data/wn18/wordnet-mlj12-test.txt',
+           '--lr', '0.1',
+           '--model', 'TransE',
+           '--similarity', 'l1',
+           '--margin', '2',
+           '--embedding-size', '50',
+           '--nb-epochs', '1000']
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+
+    # Hits@10 should be at least 90% even after a limited number of epochs
+    assert float(err.split()[-1][:-1]) > 92.0
+
 
 if __name__ == '__main__':
     pytest.main([__file__])
