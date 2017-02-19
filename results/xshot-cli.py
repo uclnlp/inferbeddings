628982eae366b11dfb6d99c02418c4a924b15a35
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-02-19 18:37:35 +0100

    towards extracting results from logs

--- results/xshot-cli.py
+++ results/xshot-cli.py
@@ -29,13 +29,25 @@ def main(argv):
     if args.base is not None:
         os.chdir(args.base)
 
-    for model_name in ['DistMult', 'ComplEx']:
+    for model_name in ['TransE', 'DistMult', 'ComplEx']:
         for sample_size in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
+
+            mr = []
+            mrr = []
+
+            hits_at_1 = []
+            hits_at_3 = []
+            hits_at_5 = []
+            hits_at_10 = []
+
+            similarity_name = 'dot'
+
             cmd = './tools/parse_results_filtered.sh ' \
-                  'logs/ucl_fb15k_adv_v1/*_subsample_size={}.log'.format(sample_size)
+                  '{}/*model={}*similarity={}*subsample_size={}.log'.format(args.logs, model_name, similarity_name, sample_size)
 
             p = subprocess.Popen(['sh', '-c', cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out, err = p.communicate()
+
 
 
 if __name__ == '__main__':
