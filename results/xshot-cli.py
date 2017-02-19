49cf508ddf487c4fa91ecda56082e22ca02d6d79
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-02-19 18:51:03 +0100

    code

--- results/xshot-cli.py
+++ results/xshot-cli.py
@@ -16,6 +16,16 @@ def exec(cmd):
     return out
 
 
+def get_results(cmd):
+    mr = float(exec(cmd + '| grep MR: | awk \'{ print $6 }\'').strip())
+    mrr = float(exec(cmd + '| grep MRR: | awk \'{ print $6 }\'').strip())
+    hits_at_1 = float(exec(cmd + '| grep Hits@1: | awk \'{ print $6 }\'').strip())
+    hits_at_3 = float(exec(cmd + '| grep Hits@3: | awk \'{ print $6 }\'').strip())
+    hits_at_5 = float(exec(cmd + '| grep Hits@5: | awk \'{ print $6 }\'').strip())
+    hits_at_10 = float(exec(cmd + '| grep Hits@10: | awk \'{ print $6 }\'').strip())
+    return mr, mrr, hits_at_1, hits_at_3, hits_at_5, hits_at_10
+
+
 def main(argv):
     def formatter(prog):
         return argparse.HelpFormatter(prog, max_help_position=100, width=200)
@@ -35,29 +45,30 @@ def main(argv):
     if args.base is not None:
         os.chdir(args.base)
 
+    model_to_adversarial_results = {}
+    model_to_standard_results = {}
+    model_to_logic_results = {}
+    model_to_naacl_results = {}
+
     for model_name in ['TransE', 'DistMult', 'ComplEx']:
+
+        mr, mrr = [], []
+        hits_at_1, hits_at_3, hits_at_5, hits_at_10 = [], [], [], []
+
         for sample_size in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
-
-            mr = []
-            mrr = []
-
-            hits_at_1 = []
-            hits_at_3 = []
-            hits_at_5 = []
-            hits_at_10 = []
-
             similarity_name = 'dot'
-
             cmd = './tools/parse_results_filtered.sh ' \
                   '{}/*model={}*similarity={}*subsample_size={}.log'.format(args.logs, model_name, similarity_name, sample_size)
 
-            mr += [float(exec(cmd + '| grep MR: | awk \'{ print $6 }\'').strip())]
-            mrr += [float(exec(cmd + '| grep MRR: | awk \'{ print $6 }\'').strip())]
+            _mr, _mrr, _hits_at_1, _hits_at_3, _hits_at_5, _hits_at_10 = get_results(cmd)
 
-            hits_at_1 += [float(exec(cmd + '| grep Hits@1: | awk \'{ print $6 }\'').strip())]
-            hits_at_3 += [float(exec(cmd + '| grep Hits@3: | awk \'{ print $6 }\'').strip())]
-            hits_at_5 += [float(exec(cmd + '| grep Hits@5: | awk \'{ print $6 }\'').strip())]
-            hits_at_10 += [float(exec(cmd + '| grep Hits@10: | awk \'{ print $6 }\'').strip())]
+            mr += [_mr]
+            mrr += [_mrr]
+
+            hits_at_1 += [_hits_at_1]
+            hits_at_3 += [_hits_at_3]
+            hits_at_5 += [_hits_at_5]
+            hits_at_10 += [_hits_at_10]
 
 
 if __name__ == '__main__':
