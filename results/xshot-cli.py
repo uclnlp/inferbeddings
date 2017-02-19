ab422b3fb52b172830e443626cd42f59a7265a20
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-02-19 17:41:13 +0100

    code for acquiring the results

--- results/xshot-cli.py
+++ results/xshot-cli.py
@@ -16,21 +16,26 @@ def main(argv):
 
     argparser = argparse.ArgumentParser('Generate plots for X-Shot Learning', formatter_class=formatter)
 
-    argparser.add_argument('--base', action='store', type=str,
-                           default='/home/pasquale/ucl/workspace/inferbeddings',
+    argparser.add_argument('base', action='store', type=str, default='./',
                            help='Folder where "inferbeddings" is located')
+    argparser.add_argument('logs', action='store', type=str,
+                           help='Folder where the logs are located (e.g. "logs/ucl_wn18_adv_xshot_v1/")')
 
     args = argparser.parse_args(argv)
+
+    assert args.base is not None
+    assert args.logs is not None
 
     if args.base is not None:
         os.chdir(args.base)
 
-    for sample_size in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
-        cmd = './tools/parse_results_filtered.sh ' \
-              'logs/ucl_fb15k_adv_v1/*_subsample_size={}.log'.format(sample_size)
-        p = subprocess.Popen(['sh', '-c', './tools/parse_results_filtered.sh logs/ucl_fb15k_adv_v1/*.log'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
+    for model_name in ['DistMult', 'ComplEx']:
+        for sample_size in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
+            cmd = './tools/parse_results_filtered.sh ' \
+                  'logs/ucl_fb15k_adv_v1/*_subsample_size={}.log'.format(sample_size)
+
+            p = subprocess.Popen(['sh', '-c', cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = p.communicate()
 
 
 if __name__ == '__main__':
