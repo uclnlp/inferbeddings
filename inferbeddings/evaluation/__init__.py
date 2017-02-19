68fccb227b283948983c6eb5de8a430fbc226ed2
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-02-19 21:11:09 +0100

    readability

--- inferbeddings/evaluation/__init__.py
+++ inferbeddings/evaluation/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from inferbeddings.evaluation.base import evaluate_auc
-from inferbeddings.evaluation.base import evaluate_ranks
-from inferbeddings.evaluation.base import ranking_summary
+from inferbeddings.evaluation.base import evaluate_auc, evaluate_ranks, ranking_summary
 
 __all__ = ['evaluate_auc', 'evaluate_ranks', 'ranking_summary']
