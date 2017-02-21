34ab184fcfacef4ede6c3a8b324425041deb6a08
Pasquale Minervini <p.minervini@gmail.com>
2017-02-21 15:42:41 +0100

    git for generating all tex tables

---
+++ results/generate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd logs
+#ack -L "test filtered" | xargs rm
+cd ..
+
+
+./results/xshot-cli.py . logs/ucl_wn18_adv_xshot_v1/ logs/ucl_wn18_adv_xshot_logic_v1/
+./results/aggregate_results.py
+./results/tex_results.py
+mv results/results* results/model_to_* results/wn18/
+
+./results/xshot-cli.py . logs/ucl_music_adv_xshot_v1/ logs/ucl_music_adv_xshot_logic_v1/
+./results/aggregate_results.py
+./results/tex_results.py
+mv results/results* results/model_to_* results/music/
+
+./results/xshot-cli.py . logs/ucl_yago3_adv_xshot_v1/ logs/ucl_yago3_adv_xshot_logic_v1/
+./results/aggregate_results.py
+./results/tex_results.py
+mv results/results* results/model_to_* results/music/
