d47ddfbcdf6d0c3ac5492141dde93bf71abdc0e9
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-03-03 21:01:58 +0100

    utilities for loading pre-trained embeddings

--- inferbeddings/io/embeddings.py
+++ inferbeddings/io/embeddings.py
@@ -15,15 +15,40 @@ def load_glove(stream, words=None):
     :param words: Words in the existing vocabulary.
     :return: dict {word: embedding}
     """
-    logger.info('Loading GloVe embeddings ..')
-    model = {}
+    word2embedding = {}
     for n, line in enumerate(stream):
         split_line = line.decode('utf-8').split()
         word = split_line[0]
         if words is None or word in words:
-            embedding = [float(val) for val in split_line[1:]]
-            model[word] = embedding
-        if n % 1000 == 0:
-            logger.info('  {}k vectors processed...\r'.format(str(n // 1000)))
-    logger.info('Done loading GloVe embeddings.')
-    return model
+            word2embedding[word] = [float(val) for val in split_line[1:]]
+    return word2embedding
+
+
+def load_word2vec(stream, words=None):
+    """
+    Loads word2vec word embeddings.
+
+    :param stream: An opened stream to the GloVe file.
+    :param words: Words in the existing vocabulary.
+    :return: dict {word: embedding}
+    """
+    word2embedding = {}
+
+    vec_n, vec_size = map(int, stream.readline().split())
+    byte_size = vec_size * 4
+
+    for n in range(vec_n):
+        word = b''
+
+        while True:
+            c = stream.read(1)
+            if c == b' ':
+                break
+            else:
+                word += c
+        word = word.decode('utf-8')
+
+        vector = np.fromstring(stream.read(byte_size), dtype=np.float32)
+        if words is None or word in words:
+            word2embedding[word] = vector.tolist()
+    return word2embedding
