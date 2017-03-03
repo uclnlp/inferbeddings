94b56b86e5cfe01404add8454cb36bd1e831d279
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-03-03 18:39:05 +0100

    tests

--- tests/inferbeddings/regularizers/test_regularizers.py
+++ tests/inferbeddings/regularizers/test_regularizers.py
@@ -16,7 +16,7 @@ def l2sqr(x):
 
 def complex_conjugate(x):
     x_re, x_im = np.split(x, indices_or_sections=2, axis=-1)
-    return np.concatenate((x_re, x_im), axis=-1)
+    return np.concatenate((x_re, - x_im), axis=-1)
 
 
 def test_translations():
@@ -96,12 +96,14 @@ def test_complex():
         loss = ComplExEquivalentPredicateRegularizer(x1=var[0:4, :], x2=var[0:4, :])()
         np.testing.assert_almost_equal(session.run(loss), [0.0] * 4)
 
+        loss = ComplExEquivalentPredicateRegularizer(x1=var[0, :], x2=var[1, :], is_inverse=True)()
+        np.testing.assert_almost_equal(session.run(loss), l2sqr(pe[0, :] - complex_conjugate(pe[1, :])))
+
         loss = ComplExEquivalentPredicateRegularizer(x1=var[0:4, :], x2=var[1:5, :])()
         np.testing.assert_almost_equal(session.run(loss), l2sqr(pe[0:4, :] - pe[1:5, :]))
 
-        # loss = ComplExEquivalentPredicateRegularizer(x1=var[0:4, :], x2=var[1:5, :], is_inverse=True)()
-        # np.testing.assert_almost_equal(session.run(loss), l2sqr(pe[0:4, :] - complex_conjugate(pe[1:5, :])))
-
+        loss = ComplExEquivalentPredicateRegularizer(x1=var[0:4, :], x2=var[1:5, :], is_inverse=True)()
+        np.testing.assert_almost_equal(session.run(loss), l2sqr(pe[0:4, :] - complex_conjugate(pe[1:5, :])))
 
 if __name__ == '__main__':
     pytest.main([__file__])
