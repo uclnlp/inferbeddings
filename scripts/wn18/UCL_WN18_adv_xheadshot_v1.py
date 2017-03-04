9bdd718935e8e9b2f5bdd73fcfc2b8011060c052
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-03-04 20:45:59 +0100

    6GB seem not to be enough for YAGO with TF 1.0 - increasing to 10GB

--- scripts/wn18/UCL_WN18_adv_xheadshot_v1.py
+++ scripts/wn18/UCL_WN18_adv_xheadshot_v1.py
@@ -130,7 +130,7 @@ def main(argv):
                 alias = ''
                 job_script = '#$ -S /bin/bash\n' \
                              '#$ -wd /tmp/\n' \
-                             '#$ -l h_vmem=10G,tmem=10G\n' \
+                             '#$ -l h_vmem=8G,tmem=8G\n' \
                              '#$ -l h_rt=24:00:00\n' \
                              '{}\n{}\n'.format(alias, line)
 
