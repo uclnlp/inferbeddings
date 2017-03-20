a89ae626e42b545c1b3fef0c63b94bc7f8387671
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-03-20 15:22:17 +0100

    closed form solutions for TransE, simple implications

--- inferbeddings/adversarial/closedform/lifted.py
+++ inferbeddings/adversarial/closedform/lifted.py
@@ -20,10 +20,13 @@ class ClosedForm:
         self.model_class, self.model_parameters = model_class, model_parameters
         self.is_unit_cube = is_unit_cube
 
-    def _distmult_loss(self, clause):
+    def _to_idx(self, predicate_name):
+        return self.parser.predicate_to_index[predicate_name]
+
+    def _translating_loss(self, clause):
         head, body = clause.head, clause.body
 
-        # At the moment, only simple rules as in "q(X, Y) :- p(X, Y)" are supported
+        # At the moment, only simple rules as in "r(X, Y) :- b(X, Y)" are supported
         assert len(body) == 1
         body_atom = body[0]
 
@@ -31,23 +34,45 @@ class ClosedForm:
         assert len(variable_names) == 2
 
         # Indices of q and r, respectively
-        head_predicate_idx = self.parser.predicate_to_index[head.predicate.name]
-        body_predicate_idx = self.parser.predicate_to_index[body_atom.predicate.name]
+        r_idx, b_idx = self._to_idx[head.predicate.name], self._to_idx[body_atom.predicate.name]
 
-        head_predicate_emb = tf.nn.embedding_lookup(self.predicate_embedding_layer, head_predicate_idx)
-        body_predicate_emb = tf.nn.embedding_lookup(self.predicate_embedding_layer, body_predicate_idx)
+        r = tf.nn.embedding_lookup(self.predicate_embedding_layer, r_idx)
+        b = tf.nn.embedding_lookup(self.predicate_embedding_layer, b_idx)
+
+        prefix = tf.reduce_sum(tf.square(b)) - tf.reduce_sum(tf.square(r))
+        if self.is_unit_cube:
+            loss = tf.nn.relu(prefix + 2 * tf.reduce_sum(tf.abs(b - r)))
+        else:
+            loss = tf.nn.relu(prefix + 4 * tf.sqrt(tf.reduce_sum(tf.square(b - r))))
+        return loss
+
+    def _bilinear_diagonal_loss(self, clause):
+        head, body = clause.head, clause.body
+
+        # At the moment, only simple rules as in "r(X, Y) :- b(X, Y)" are supported
+        assert len(body) == 1
+        body_atom = body[0]
+
+        variable_names = {arg.name for arg in head.arguments} | {arg.name for arg in body_atom.arguments}
+        assert len(variable_names) == 2
+
+        # Indices of q and r, respectively
+        r_idx, b_idx = self._to_idx[head.predicate.name], self._to_idx[body_atom.predicate.name]
+
+        r = tf.nn.embedding_lookup(self.predicate_embedding_layer, r_idx)
+        b = tf.nn.embedding_lookup(self.predicate_embedding_layer, b_idx)
 
         if self.is_unit_cube:
-            loss = tf.reduce_sum(tf.nn.relu(body_predicate_emb - head_predicate_emb))
+            loss = tf.reduce_sum(tf.nn.relu(b - r))
         else:
-            loss = tf.reduce_max(tf.abs(body_predicate_emb - head_predicate_emb))
+            loss = tf.reduce_max(tf.abs(b - r))
         return loss
 
     def __call__(self, clause):
         loss = None
         if self.model_class == BilinearDiagonalModel:
             # We are using DistMult
-            loss = self._distmult_loss(clause)
+            loss = self._bilinear_diagonal_loss(clause)
 
         assert loss is not None
         return loss
