1cc46cc6ba30e5968dcaf2cc1f5324f91d11cf21
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-03-24 13:00:57 +0100

    more tests

--- tests/inferbeddings/test_adv_cli.py
+++ tests/inferbeddings/test_adv_cli.py
@@ -8,6 +8,29 @@ sys.setrecursionlimit(65535)
 
 
 def test_symmetric_tiny_cli():
+    # Checking if results are still not nice when not using adversarial training,
+    # i.e. by using the parameters --adv-weight 0
+    cmd = ['./bin/adv-cli.py',
+           '--train', 'data/synth/symmetric-tiny/data.tsv',
+           '--test', 'data/synth/symmetric-tiny/data-inverse-test.tsv',
+           '--lr', '0.1',
+           '--model', 'ComplEx',
+           '--similarity', 'dot',
+           '--margin', '1',
+           '--embedding-size', '50',
+           '--nb-epochs', '100',
+           '--clauses', 'data/synth/symmetric-tiny/clauses_inverse.pl',
+           '--adv-lr', '0.1',
+           '--adv-batch-size', '100',
+           '--adv-weight', '0',
+           '--adversary-epochs', '10',
+           '--loss', 'hinge']
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+
+    # Hits@10 should be around 0% at first
+    assert float(err.split()[-1][:-1]) < 10.0
+
     # Checking if results are still nice
     cmd = ['./bin/adv-cli.py',
            '--train', 'data/synth/symmetric-tiny/data.tsv',
