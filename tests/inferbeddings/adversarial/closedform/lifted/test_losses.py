fe6a10de6f055de511287b47ed76362385e69dcb
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-03-26 20:21:58 +0200

    check that the adversarial loss never increases

--- tests/inferbeddings/adversarial/closedform/lifted/test_losses.py
+++ tests/inferbeddings/adversarial/closedform/lifted/test_losses.py
@@ -12,6 +12,7 @@ from inferbeddings.models.training import constraints
 from inferbeddings.adversarial.ground import GroundLoss
 from inferbeddings.adversarial.closedform import ClosedFormLifted
 
+import itertools
 import logging
 
 import pytest
@@ -20,98 +21,112 @@ logger = logging.getLogger(__name__)
 
 triples = [
     ('a', 'p', 'b'),
-    ('c', 'p', 'd'),
     ('a', 'q', 'b')
 ]
 facts = [Fact(predicate_name=p, argument_names=[s, o]) for s, p, o in triples]
 parser = KnowledgeBaseParser(facts)
 
+a_idx = parser.entity_to_index['a']
+b_idx = parser.entity_to_index['b']
+
 nb_entities = len(parser.entity_to_index)
 nb_predicates = len(parser.predicate_to_index)
 
 # Clauses
-clause_str = 'q(X, Y) :- p(Y, X)'
-clauses = [parse_clause(clause_str)]
+clause = parse_clause('q(X, Y) :- p(X, Y)')
 
-# Instantiating the model parameters
-model_class = models.get_function('ComplEx')
-similarity_function = similarities.get_function('dot')
 
-unit_cube = False
+def cartesian_product(dicts):
+    return (dict(zip(dicts, x)) for x in itertools.product(*dicts.values()))
+
+hyperparams = {
+    'unit_cube': [True, False],
+    'model_name': ['DistMult', 'ComplEx']
+}
 
 
 @pytest.mark.closedform
 def test_losses():
-    for seed in range(1):
-        tf.reset_default_graph()
 
-        np.random.seed(seed)
-        tf.set_random_seed(seed)
+    hyperparam_configurations = list(cartesian_product(hyperparams))
 
-        entity_embedding_size = np.random.randint(low=1, high=5) * 2
-        predicate_embedding_size = entity_embedding_size
+    for hyperparam_configuration in hyperparam_configurations:
 
-        # Instantiating entity and predicate embedding layers
-        entity_embedding_layer = tf.get_variable('entities',
-                                                 shape=[nb_entities + 1, entity_embedding_size],
-                                                 initializer=tf.contrib.layers.xavier_initializer())
+        # Instantiating the model parameters
+        model_class = models.get_function(hyperparam_configuration['model_name'])
+        similarity_function = similarities.get_function('dot')
 
-        predicate_embedding_layer = tf.get_variable('predicates',
-                                                    shape=[nb_predicates + 1, predicate_embedding_size],
-                                                    initializer=tf.contrib.layers.xavier_initializer())
+        unit_cube = hyperparam_configuration['unit_cube']
 
-        entity_projection = constraints.unit_sphere(entity_embedding_layer, norm=1.0)
-        if unit_cube:
-            entity_projection = constraints.unit_cube(entity_embedding_layer)
+        for seed in range(4):
+            print('Seed {}, Evaluating {}'.format(seed, str(hyperparam_configuration)))
 
-        entity_inputs = tf.placeholder(tf.int32, shape=[None, 2])
-        walk_inputs = tf.placeholder(tf.int32, shape=[None, None])
+            tf.reset_default_graph()
 
-        entity_embeddings = tf.nn.embedding_lookup(entity_embedding_layer, entity_inputs)
-        predicate_embeddings = tf.nn.embedding_lookup(predicate_embedding_layer, walk_inputs)
+            np.random.seed(seed)
+            tf.set_random_seed(seed)
 
-        model_parameters = dict(entity_embeddings=entity_embeddings,
-                                predicate_embeddings=predicate_embeddings,
-                                similarity_function=similarity_function)
+            entity_embedding_size = np.random.randint(low=1, high=5) * 2
+            predicate_embedding_size = entity_embedding_size
 
-        model = model_class(**model_parameters)
-        score = model()
+            # Instantiating entity and predicate embedding layers
+            entity_embedding_layer = tf.get_variable('entities',
+                                                     shape=[nb_entities + 1, entity_embedding_size],
+                                                     initializer=tf.contrib.layers.xavier_initializer())
 
-        closed_form_lifted = ClosedFormLifted(parser=parser,
-                                              predicate_embedding_layer=predicate_embedding_layer,
-                                              model_class=model_class,
-                                              model_parameters=model_parameters,
-                                              is_unit_cube=unit_cube)
-        opt_adversarial_loss = closed_form_lifted(clauses[0])
+            predicate_embedding_layer = tf.get_variable('predicates',
+                                                        shape=[nb_predicates + 1, predicate_embedding_size],
+                                                        initializer=tf.contrib.layers.xavier_initializer())
 
-        v_optimizer = tf.train.AdagradOptimizer(learning_rate=1e-1)
-        v_training_step = v_optimizer.minimize(opt_adversarial_loss, var_list=[predicate_embedding_layer])
+            entity_projection = constraints.unit_sphere(entity_embedding_layer, norm=1.0)
+            if unit_cube:
+                entity_projection = constraints.unit_cube(entity_embedding_layer)
 
-        init_op = tf.global_variables_initializer()
+            entity_inputs = tf.placeholder(tf.int32, shape=[None, 2])
+            walk_inputs = tf.placeholder(tf.int32, shape=[None, None])
 
-        with tf.Session() as session:
+            entity_embeddings = tf.nn.embedding_lookup(entity_embedding_layer, entity_inputs)
+            predicate_embeddings = tf.nn.embedding_lookup(predicate_embedding_layer, walk_inputs)
 
-            def scoring_function(args):
-                return session.run(score, feed_dict={walk_inputs: args[0], entity_inputs: args[1]})
+            model_parameters = dict(entity_embeddings=entity_embeddings,
+                                    predicate_embeddings=predicate_embeddings,
+                                    similarity_function=similarity_function)
 
-            ground_loss = GroundLoss(clauses=clauses, parser=parser, scoring_function=scoring_function)
+            model = model_class(**model_parameters)
+            score = model()
 
-            entity_indices = sorted({idx for idx in parser.entity_to_index.values()})
-            feed_dicts = GroundLoss.sample_mappings(GroundLoss.get_variable_names(clauses[0]),
-                                                    entities=entity_indices,
-                                                    sample_size=100)
+            closed_form_lifted = ClosedFormLifted(parser=parser,
+                                                  predicate_embedding_layer=predicate_embedding_layer,
+                                                  model_class=model_class,
+                                                  model_parameters=model_parameters,
+                                                  is_unit_cube=unit_cube)
+            opt_adversarial_loss = closed_form_lifted(clause)
 
-            session.run(init_op)
+            v_optimizer = tf.train.AdagradOptimizer(learning_rate=1e-2)
+            v_training_step = v_optimizer.minimize(opt_adversarial_loss, var_list=[predicate_embedding_layer])
 
-            continuous_error_0 = ground_loss.continuous_errors(clauses[0], feed_dicts=feed_dicts)
+            init_op = tf.global_variables_initializer()
 
-            for finding_epoch in range(1, 100 + 1):
-                _ = session.run([v_training_step])
+            with tf.Session() as session:
+                session.run(init_op)
+
                 session.run([entity_projection])
 
-            continuous_error_final = ground_loss.continuous_errors(clauses[0], feed_dicts=feed_dicts)
+                def scoring_function(args):
+                    return session.run(score, feed_dict={walk_inputs: args[0], entity_inputs: args[1]})
 
-            assert continuous_error_final <= continuous_error_0
+                ground_loss = GroundLoss(clauses=[clause], parser=parser, scoring_function=scoring_function)
+                feed_dicts = [{'X': a_idx, 'Y': b_idx}]
+
+                continuous_loss_0 = ground_loss.continuous_errors(clause, feed_dicts=feed_dicts)
+
+                for epoch in range(1, 100 + 1):
+                    _ = session.run([v_training_step])
+                    print(ground_loss.continuous_errors(clause, feed_dicts=feed_dicts))
+
+                continuous_loss_final = ground_loss.continuous_errors(clause, feed_dicts=feed_dicts)
+
+                assert continuous_loss_0 <= .0 or continuous_loss_final <= continuous_loss_0
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
