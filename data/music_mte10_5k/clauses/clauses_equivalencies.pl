532b11661cc1f54e13ad2eb2ee4305ce3acfbb0b
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-04-04 20:02:30 +0200

    clauses set encoding equivalencies between predicates

---
+++ data/music_mte10_5k/clauses/clauses_equivalencies.pl
@@ -0,0 +1,2 @@
+"<http://dbpedia.org/ontology/associatedBand>"(X0, X1) :- "<http://dbpedia.org/ontology/associatedMusicalArtist>"(X0, X1)
+"<http://dbpedia.org/ontology/musicalArtist>"(X0, X1) :- "<http://dbpedia.org/ontology/musicalBand>"(X0, X1)
---
+++ data/yago3_mte10_5k/clauses/clauses_equivalencies.pl
@@ -0,0 +1,2 @@
+isMarriedTo(X0, X1) :- isMarriedTo(X1, X0)
+hasNeighbor(X0, X1) :- hasNeighbor(X1, X0)
