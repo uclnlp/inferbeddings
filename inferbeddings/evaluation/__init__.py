7288349b6e2a4ba5f6ed692ede0eda2b78722950
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-04-05 18:59:27 +0200

    small fix - forgot to expose evaluate_map

--- inferbeddings/evaluation/__init__.py
+++ inferbeddings/evaluation/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from inferbeddings.evaluation.base import evaluate_auc, evaluate_ranks, ranking_summary
+from inferbeddings.evaluation.base import evaluate_auc, evaluate_ranks, evaluate_map, ranking_summary
 
 __all__ = ['evaluate_auc',
            'evaluate_ranks',
