f7a6f4c9da4bf4f304cdc9ab4e7b4762ef61001d
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-05-01 20:03:16 +0200

    parsing Countries experiments

--- tools/parse_results_countries.py
+++ tools/parse_results_countries.py
@@ -5,12 +5,20 @@ import glob
 import os
 import sys
 
+import numpy as np
 from tqdm import tqdm
 
 import argparse
 import logging
 
 logger = logging.getLogger(os.path.basename(sys.argv[0]))
+
+
+def stats(values):
+    print(len(values))
+    if len(values) != 10:
+        return '0'
+    return '{0:.4f} ± {1:.4f}'.format(round(np.mean(values), 4), round(np.std(values), 4))
 
 
 def main(argv):
@@ -25,7 +33,7 @@ def main(argv):
 
     path_to_valid_aucpr, path_to_test_aucpr = {}, {}
 
-    for file_path in tqdm(glob.glob('{}/*.log'.format(path))):
+    for file_path in tqdm(glob.glob('{}/*_model=DistMult_*_unit_cube=False*.log'.format(path))):
         with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
             for line in f:
                 if '[valid]' in line and 'AUC-PR' in line:
@@ -33,11 +41,14 @@ def main(argv):
                 if '[test]' in line and 'AUC-PR' in line:
                     path_to_test_aucpr[file_path] = line
 
-    path_set = set(path_to_valid_aucpr.keys()) | set(path_to_test_aucpr.keys())
+    path_set = set(path_to_valid_aucpr.keys()) & set(path_to_test_aucpr.keys())
+
+    # Use the following for debugging:
+    # path_set = set(path_to_valid_aucpr.keys()) | set(path_to_test_aucpr.keys())
 
     new_path_to_valid_aucprs, new_path_to_test_aucprs = {}, {}
 
-    for path in path_set:
+    for path in tqdm(path_set):
         _new_path = path
         for i in range(10):
             _new_path = _new_path.replace('_seed={}'.format(i), '_seed=X')
@@ -50,7 +61,22 @@ def main(argv):
         new_path_to_valid_aucprs[_new_path] += [path_to_valid_aucpr[path]]
         new_path_to_test_aucprs[_new_path] += [path_to_test_aucpr[path]]
 
+    new_paths = set(new_path_to_valid_aucprs.keys()) & set(new_path_to_test_aucprs.keys())
+    new_path_to_valid_aucpr_stats, new_path_to_test_aucpr_stats = {}, {}
 
+    for _new_path in new_paths:
+        def tfl(line):
+            x, y, z = line.split()
+            return float(z)
+
+        new_path_to_valid_aucpr_stats[_new_path] = stats([tfl(l) for l in new_path_to_valid_aucprs[_new_path]])
+        new_path_to_test_aucpr_stats[_new_path] = stats([tfl(l) for l in new_path_to_test_aucprs[_new_path]])
+
+    #for p, s in new_path_to_valid_aucpr_stats.items():
+    #    print('{}\t{}\t[VALID]'.format(s, p))
+
+    #for p, s in new_path_to_test_aucpr_stats.items():
+    #    print('{}\t{}\t[TEST]'.format(s, p))
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
