1bab0c391e4b82c7d0cfee087c372d0a07b833e3
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-05-04 17:33:35 +0200

    pre-trained embeddings, test cases

--- inferbeddings/io/embeddings.py
+++ inferbeddings/io/embeddings.py
@@ -15,13 +15,19 @@ def load_glove(stream, words=None):
     :param words: Words in the existing vocabulary.
     :return: dict {word: embedding}
     """
-    word2embedding = {}
+    word_to_embedding = {}
+
     for n, line in enumerate(stream):
-        split_line = line.decode('utf-8').split()
+        split_line = line.split(' ')
         word = split_line[0]
+
         if words is None or word in words:
-            word2embedding[word] = [float(val) for val in split_line[1:]]
-    return word2embedding
+            try:
+                word_to_embedding[word] = [float(f) for f in split_line[1:]]
+            except ValueError:
+                logger.error('{}\t{}\t{}'.format(n, word, str(split_line)))
+
+    return word_to_embedding
 
 
 def load_word2vec(stream, words=None):
@@ -32,7 +38,7 @@ def load_word2vec(stream, words=None):
     :param words: Words in the existing vocabulary.
     :return: dict {word: embedding}
     """
-    word2embedding = {}
+    word_to_embedding = {}
 
     vec_n, vec_size = map(int, stream.readline().split())
     byte_size = vec_size * 4
@@ -46,8 +52,11 @@ def load_word2vec(stream, words=None):
                 break
             else:
                 word += c
-        word = word.decode('utf-8')
-        vector = np.fromstring(stream.read(byte_size), dtype=np.float32)
         if words is None or word in words:
-            word2embedding[word] = vector.tolist()
-    return word2embedding
+            try:
+                vector = np.fromstring(stream.read(byte_size), dtype=np.float32)
+                word_to_embedding[word] = vector.tolist()
+            except ValueError:
+                logger.error('{}\t{}\t{}'.format(n, word, str(byte_size)))
+
+    return word_to_embedding
--- tests/inferbeddings/io/test_io.py
+++ tests/inferbeddings/io/test_io.py
@@ -1,22 +1,51 @@
 # -*- coding: utf-8 -*-
 
 import pytest
+
+import os
+import sys
+
+import numpy as np
+
 from inferbeddings.io import iopen, load_glove, load_word2vec
 
 
+def test_load_glove_large():
+    glove_path = os.path.expanduser('~/data/glove/glove.840B.300d.txt')
+    if os.path.isfile(glove_path):
+        word_set = {'machine', 'learning'}
+        with open(glove_path, 'r') as stream:
+            word_to_embedding = load_glove(stream=stream, words=word_set)
+
+        machine_vector = word_to_embedding['machine']
+        learning_vector = word_to_embedding['learning']
+
+        np.testing.assert_allclose(machine_vector[1:4], [-0.12538, 0.38888, 0.48011], rtol=1e-3)
+        np.testing.assert_allclose(learning_vector[1:4], [0.047511, 0.1404, -0.11736], rtol=1e-3)
+
+
+def test_load_word2vec():
+    word2vec_path = os.path.expanduser('~/data/word2vec/GoogleNews-vectors-negative300.bin.gz')
+    if os.path.isfile(word2vec_path):
+        word_set = {'machine', 'learning'}
+        with iopen('data/word2vec/GoogleNews-vectors-negative300.bin.gz') as stream:
+            word_to_embedding = load_word2vec(stream=stream, words=word_set)
+
+        machine_vector = word_to_embedding['machine']
+        learning_vector = word_to_embedding['learning']
+
+        print(machine_vector)
+        print(learning_vector)
+
+
 def test_load_glove():
-    with iopen('data/glove/glove.6B.50d.txt.gz', 'r') as f:
-        model = load_glove(f, {'house'})
+    glove_path = 'data/glove/glove.6B.50d.txt.gz'
+    if os.path.isfile(glove_path):
+        with iopen(glove_path, 'r') as f:
+            model = load_glove(f, {'house'})
+        assert 'house' in model
+        assert 0.60136 < model['house'][0] < 0.60138
 
-    assert 'house' in model
-    assert 0.60136 < model['house'][0] < 0.60138
-
-
-# def test_load_word2vec():
-#     with iopen('data/word2vec/GoogleNews-vectors-negative300.bin.gz') as f:
-#         model = load_word2vec(f, {'house'})
-#     assert 'house' in model
-#     print(model['house'])
 
 if __name__ == '__main__':
     pytest.main([__file__])
