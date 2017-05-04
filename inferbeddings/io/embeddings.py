de3fae35bb0379a4567674756bea6d29e1391630
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-05-04 19:17:41 +0200

    pre-trained embeddings - loading and testing

--- inferbeddings/io/embeddings.py
+++ inferbeddings/io/embeddings.py
@@ -56,7 +56,7 @@ def load_word2vec(stream, words=None):
                 word += c
 
         if not isinstance(word, str):
-            print(word)
+            print(word, type(word))
             word = word.decode('utf-8')
 
         if words is None or word in words:
