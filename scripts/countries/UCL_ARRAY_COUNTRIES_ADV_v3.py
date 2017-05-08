ef6d2a7032948718496819b7516ba4eba6ebc6c9
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-05-08 13:08:55 +0200

    let's skip RESCAL and ERMLP at this stage

--- scripts/countries/UCL_ARRAY_COUNTRIES_ADV_v3.py
+++ scripts/countries/UCL_ARRAY_COUNTRIES_ADV_v3.py
@@ -137,6 +137,7 @@ def main(argv):
     configurations = list(cartesian_product(hyperparameters_space_1)) +\
                      list(cartesian_product(hyperparameters_space_2)) +\
                      list(cartesian_product(hyperparameters_space_3))
+    # TODO - add back ERMLP and RESCAL if there's time/need
     configurations = list(cartesian_product(hyperparameters_space_2))
 
     path = '/home/pminervi/workspace/inferbeddings/logs/ucl_countries_adv_v3/'
