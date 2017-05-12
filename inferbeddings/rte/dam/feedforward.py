a86bf515d5721553d9b3b63421d660b838814fdf
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-05-12 17:11:40 +0200

    RTE - MLP DAM variant

--- inferbeddings/rte/dam/feedforward.py
+++ inferbeddings/rte/dam/feedforward.py
@@ -12,37 +12,37 @@ class FeedForwardDAM(AbstractDecomposableAttentionModel):
         super().__init__(*args, **kwargs)
 
     def _transform_embeddings(self, embeddings, reuse=False):
-        with tf.variable_scope('transform_embeddings', reuse=reuse) as scope:
+        with tf.variable_scope('transform_embeddings', reuse=reuse) as _:
             projection = tf.contrib.layers.fully_connected(inputs=embeddings, num_outputs=self.representation_size,
                                                            weights_initializer=tf.random_normal_initializer(0.0, 0.1),
                                                            biases_initializer=tf.zeros_initializer(),
-                                                           activation_fn=None, scope=scope)
+                                                           activation_fn=None)
         return projection
 
     def _transform_attend(self, sequence, reuse=False):
-        with tf.variable_scope('transform_attend', reuse=reuse) as scope:
+        with tf.variable_scope('transform_attend', reuse=reuse) as _:
             projection = tf.contrib.layers.fully_connected(inputs=sequence, num_outputs=self.representation_size,
                                                            weights_initializer=tf.random_normal_initializer(0.0, 0.1),
                                                            biases_initializer=tf.zeros_initializer(),
-                                                           activation_fn=tf.nn.relu, scope=scope)
+                                                           activation_fn=tf.nn.relu)
             projection = tf.nn.dropout(projection, keep_prob=self.dropout_keep_prob)
             projection = tf.contrib.layers.fully_connected(inputs=projection, num_outputs=self.representation_size,
                                                            weights_initializer=tf.random_normal_initializer(0.0, 0.1),
                                                            biases_initializer=tf.zeros_initializer(),
-                                                           activation_fn=tf.nn.relu, scope=scope)
+                                                           activation_fn=tf.nn.relu)
             projection = tf.nn.dropout(projection, keep_prob=self.dropout_keep_prob)
         return projection
 
     def _transform_compare(self, sequence, reuse=False):
-        with tf.variable_scope('transform_compare', reuse=reuse) as scope:
+        with tf.variable_scope('transform_compare', reuse=reuse) as _:
             projection = tf.contrib.layers.fully_connected(inputs=sequence, num_outputs=self.representation_size,
                                                            weights_initializer=tf.random_normal_initializer(0.0, 0.1),
                                                            biases_initializer=tf.zeros_initializer(),
-                                                           activation_fn=tf.nn.relu, scope=scope)
+                                                           activation_fn=tf.nn.relu)
             projection = tf.nn.dropout(projection, keep_prob=self.dropout_keep_prob)
             projection = tf.contrib.layers.fully_connected(inputs=projection, num_outputs=self.representation_size,
                                                            weights_initializer=tf.random_normal_initializer(0.0, 0.1),
                                                            biases_initializer=tf.zeros_initializer(),
-                                                           activation_fn=tf.nn.relu, scope=scope)
+                                                           activation_fn=tf.nn.relu)
             projection = tf.nn.dropout(projection, keep_prob=self.dropout_keep_prob)
         return projection
