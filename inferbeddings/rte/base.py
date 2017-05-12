0e79f59f30ab48c61ad81cea378de93fd4f50dfd
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-05-12 18:15:47 +0200

    RTE - MLP DAM variant

--- inferbeddings/rte/base.py
+++ inferbeddings/rte/base.py
@@ -61,7 +61,7 @@ class ConditionalBiLSTM:
         labels = tf.one_hot(self.label, self.num_classes)
 
         self.losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=labels)
-        self.loss = tf.reduce_mean(self.losses, axis=1)
+        self.loss = tf.reduce_mean(self.losses)
 
         if l2_lambda is not None:
             regularizer = l2_lambda * sum(tf.nn.l2_loss(var) for var in tf.trainable_variables()
