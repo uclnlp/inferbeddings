ba6ca43a75d9d72d1f152c7768ea9fd4b34ca892
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-05-12 21:44:05 +0200

    RTE - MLP DAM variant

--- bin/snli-cli.py
+++ bin/snli-cli.py
@@ -262,8 +262,9 @@ def main(argv):
                 _, loss_value = session.run([model.training_step, model.loss], feed_dict=batch_feed_dict)
                 loss_values += [loss_value]
 
-                for projection_step in projection_steps:
-                    session.run([projection_step])
+                if not is_fixed_embeddings:
+                    for projection_step in projection_steps:
+                        session.run([projection_step])
 
                 if (batch_idx > 0 and batch_idx % 100 == 0) or (batch_start, batch_end) in batches[-1:]:
                     dev_accuracy = session.run(accuracy, feed_dict=to_feed_dict(model, dev_dataset))
