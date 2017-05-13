3df10ac3cb4aaa8bcc1d4738f151eae9efdafd7e
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-05-13 17:10:53 +0200

    testing DAM

--- tests/inferbeddings/rte/dam/test_dam.py
+++ tests/inferbeddings/rte/dam/test_dam.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
 import tensorflow as tf
 
 from inferbeddings.rte.dam import FeedForwardDAM
@@ -15,7 +16,7 @@ import pytest
 def test_ff_dam():
     vocab_size = 1000
     embedding_size = 32
-    representation_size = 16,
+    representation_size = 16
     dropout_keep_prob = 1.0
     is_fixed_embeddings = False
     learning_rate = 0.1
@@ -35,7 +36,22 @@ def test_ff_dam():
 
     with tf.Session(config=session_config) as session:
         session.run(init_op)
-        logger.debug('Total parameters: {}'.format(count_parameters()))
+        nb_parameters = count_parameters()
+
+        sentence = [[1, 2, 3]]
+        sentence_length = [3]
+
+        feed_dict = {
+            model.sentence1: sentence, model.sentence2: sentence,
+            model.sentence1_size: sentence_length, model.sentence2_size: sentence_length}
+
+        embedded1_value = session.run(model.embedded1, feed_dict=feed_dict)
+        embedded2_value = session.run(model.embedded2, feed_dict=feed_dict)
+
+        np.testing.assert_allclose(embedded1_value, embedded2_value)
+
+        alpha_value, beta_value = embedded2_value = session.run([model.alpha, model.beta], feed_dict=feed_dict)
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
