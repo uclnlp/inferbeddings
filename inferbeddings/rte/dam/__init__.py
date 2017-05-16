17e873b706548f2b68912ec35d79a1cb46bec587
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-05-16 16:35:56 +0200

    trying out DAM with PRELUs

--- inferbeddings/rte/dam/__init__.py
+++ inferbeddings/rte/dam/__init__.py
@@ -3,6 +3,7 @@
 from inferbeddings.rte.dam.base import AbstractDecomposableAttentionModel
 from inferbeddings.rte.dam.simple import SimpleDAM
 from inferbeddings.rte.dam.feedforward import FeedForwardDAM
+from inferbeddings.rte.dam.damp import DAMP
 
 __all__ = [
     'AbstractDecomposableAttentionModel',
