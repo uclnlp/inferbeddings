5ad132744dbe3e3dba656fe94f8dd93da0940a12
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-06-03 19:21:48 +0200

    first adversarial constraint for RTE

--- inferbeddings/rte2/regularizers/adversarial.py
+++ inferbeddings/rte2/regularizers/adversarial.py
@@ -2,14 +2,64 @@
 
 import tensorflow as tf
 
+
 class Adversarial:
+    """
+    Utility class for generating adversarial sets for RTE.
+    """
+
     def __init__(self, model_class, model_kwargs,
+                 embedding_size=300, batch_size=1024, sequence_length=10,
                  entailment_idx=0, contradiction_idx=1, neutral_idx=2):
         self.model_class = model_class
         self.model_kwargs = model_kwargs
+
+        self.embedding_size = embedding_size
+        self.batch_size = batch_size
+        self.sequence_length = sequence_length
 
         self.entailment_idx = entailment_idx
         self.contradiction_idx = contradiction_idx
         self.neutral_idx = neutral_idx
 
-        
+        self.variable_name_to_variable = dict()
+
+    def rule1(self):
+        sequence1 = tf.get_variable('rule1_sequence1',
+                                    shape=[self.batch_size, self.sequence_length, self.embedding_size],
+                                    initializer=tf.contrib.layers.xavier_initializer())
+        self.variable_name_to_variable['rule1_sequence1'] = sequence1
+
+        sequence2 = tf.get_variable('rule1_sequence2',
+                                    shape=[self.batch_size, self.sequence_length, self.embedding_size],
+                                    initializer=tf.contrib.layers.xavier_initializer())
+        self.variable_name_to_variable['rule1_sentence2_embedding'] = sequence2
+
+        a_model_kwargs = self.model_kwargs.copy()
+
+        a_model_kwargs['sequence1'] = sequence1
+        a_model_kwargs['sequence1_length'] = self.sequence_length
+
+        a_model_kwargs['sequence2'] = sequence2
+        a_model_kwargs['sequence2_length'] = self.sequence_length
+
+        a_model = self.model_class(**a_model_kwargs)
+        a_logits = a_model()
+
+        p_s1_entails_s2 = tf.nn.softmax(a_logits)[:, self.contradiction_idx]
+
+        b_model_kwargs = self.model_kwargs.copy()
+
+        b_model_kwargs['sequence1'] = sequence2
+        b_model_kwargs['sequence1_length'] = self.sequence_length
+
+        b_model_kwargs['sequence2'] = sequence1
+        b_model_kwargs['sequence2_length'] = self.sequence_length
+
+        b_model = self.model_class(**b_model_kwargs)
+        b_logits = b_model()
+
+        p_s2_entails_s1 = tf.nn.softmax(b_logits)[:, self.contradiction_idx]
+
+        return tf.nn.l2_loss(p_s1_entails_s2 - p_s2_entails_s1)
+
