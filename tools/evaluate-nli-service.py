d875c1fc916d08f4ef0f2e57205337f056329cc0
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-06-06 20:43:31 +0200

    better code

--- tools/evaluate-nli-service.py
+++ tools/evaluate-nli-service.py
@@ -35,7 +35,7 @@ def main(argv):
     for instance in instances:
         question, support, answer = instance['question'], instance['support'], instance['answer']
         res = session.post('http://127.0.0.1:8889/v1/nli', data={'sentence2': question, 'sentence1': support})
-        prediction = sorted(res.json().items(), key=operator.itemgetter(1))[0][0]
+        prediction = sorted(res.json().items(), key=operator.itemgetter(1), reverse=True)[0][0]
         print(answer, prediction)
 
 if __name__ == '__main__':
