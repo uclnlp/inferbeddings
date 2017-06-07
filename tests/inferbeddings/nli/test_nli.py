f83ff14180bd5bca994bf00134ea3df56f0b1184
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-06-07 13:56:46 +0200

    update to tests

--- tests/inferbeddings/nli/test_nli.py
+++ tests/inferbeddings/nli/test_nli.py
@@ -14,6 +14,7 @@ logger = logging.getLogger(__name__)
 
 def test_nli_damp():
     embedding_size = 300
+    representation_size = 200
     max_len = None
 
     train_instances, dev_instances, test_instances = util.SNLI.generate(bos='<BOS>', eos='<EOS>')
@@ -32,4 +33,27 @@ def test_nli_damp():
     test_dataset = util.to_dataset(test_instances, qs_tokenizer, a_tokenizer, max_len=max_len)
 
     sentence1_ph = tf.placeholder(dtype=tf.int32, shape=[None, None], name='sentence1')
+    sentence2_ph = tf.placeholder(dtype=tf.int32, shape=[None, None], name='sentence2')
 
+    sentence1_length_ph = tf.placeholder(dtype=tf.int32, shape=[None], name='sentence1_length')
+    sentence2_length_ph = tf.placeholder(dtype=tf.int32, shape=[None], name='sentence2_length')
+
+    embedding_layer = tf.get_variable('embeddings', shape=[vocab_size, embedding_size],
+                                      initializer=tf.contrib.layers.xavier_initializer(),
+                                      trainable=False)
+
+    sentence1_embedding = tf.nn.embedding_lookup(embedding_layer, sentence1_ph)
+    sentence2_embedding = tf.nn.embedding_lookup(embedding_layer, sentence2_ph)
+
+    model_kwargs = dict(
+        sequence1=sentence1_embedding, sequence1_length=sentence1_length_ph,
+        sequence2=sentence2_embedding, sequence2_length=sentence2_length_ph,
+        representation_size=representation_size, dropout_keep_prob=1.0, use_masking=True)
+    model_class = FeedForwardDAMP
+
+    model = model_class(**model_kwargs)
+
+    logits = model()
+    probabilities = tf.nn.softmax(logits)
+
+    
