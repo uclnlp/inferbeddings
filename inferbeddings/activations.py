64e521c12dec464fa68a75c6af559900ad262edb
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-06-15 00:31:27 +0200

    Adding SELU

--- inferbeddings/activations.py
+++ inferbeddings/activations.py
@@ -11,6 +11,13 @@ def parametric_relu(x, name=None):
                              dtype=tf.float32)
     return tf.nn.relu(x) + alphas * (x - abs(x)) * 0.5
 
+
+def selu(x):
+    alpha = 1.6732632423543772848170429916717
+    scale = 1.0507009873554804934193349852946
+    return scale * tf.where(x >= 0.0, x, alpha * tf.nn.elu(x))
+
+
 # Aliases
 relu = tf.nn.relu
 prelu = parametric_relu
