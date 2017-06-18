207273b636430892b56d6807e2e35b13ac51abf1
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-06-18 18:02:18 +0200

    update

--- tests/inferbeddings/nli/test_alpha_beta.py
+++ tests/inferbeddings/nli/test_alpha_beta.py
@@ -15,12 +15,8 @@ def test_nli_alpha_beta():
     rs = np.random.RandomState(0)
     weight_matrix = rs.rand(1, 5, 4)
 
-    weight_matrix_1 = np.transpose(
-        np.exp(weight_matrix - weight_matrix.max(1)[:, None, :]),
-        (1, 2, 0))
-    weight_matrix_2 = np.transpose(
-        np.exp(weight_matrix - weight_matrix.max(2)[:, :, None]),
-        (1, 2, 0))
+    weight_matrix_1 = np.transpose(np.exp(weight_matrix), (1, 2, 0))
+    weight_matrix_2 = np.transpose(np.exp(weight_matrix), (1, 2, 0))
 
     assert weight_matrix_1.shape == (5, 4, 1)
     assert weight_matrix_2.shape == (5, 4, 1)
@@ -35,10 +31,11 @@ def test_nli_alpha_beta():
     print(beta[:, :, 0])
 
     with tf.Session() as session:
-        x = session.run(tfutil.attention_softmax3d(weight_matrix))
-        np.testing.assert_allclose(x[0, :, :], beta[:, :, 0])
-        x = session.run(tfutil.attention_softmax3d(np.transpose(weight_matrix, (0, 2, 1))))
-        np.testing.assert_allclose(np.transpose(x[0, :, :], (1, 0)), alpha[:, :, 0])
+        _beta = session.run(tfutil.attention_softmax3d(weight_matrix))
+        np.testing.assert_allclose(_beta[0, :, :], beta[:, :, 0])
+
+        _alpha = session.run(tfutil.attention_softmax3d(np.transpose(weight_matrix, (0, 2, 1))))
+        np.testing.assert_allclose(np.transpose(_alpha[0, :, :], (1, 0)), alpha[:, :, 0])
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
