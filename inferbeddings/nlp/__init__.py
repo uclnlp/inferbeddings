4e0cabcaba81cbd57447542760cfaa3ed462797f
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-06-20 23:15:01 +0200

    some small refactoring

--- inferbeddings/nlp/__init__.py
+++ inferbeddings/nlp/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from inferbeddings.nlp.base import Tokenizer
-from inferbeddings.nlp.simple import SimpleTokenizer
+from inferbeddings.nlp.tokenizers.base import Tokenizer
+from inferbeddings.nlp.tokenizers.simple import SimpleTokenizer
 
 __all__ = [
     'Tokenizer',
