c4387c7969698938be6e4bf695acae3748680177
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-08-25 15:51:03 +0200

    update

--- tests/inferbeddings/test_nli_cli.py
+++ tests/inferbeddings/test_nli_cli.py
@@ -55,24 +55,24 @@ def test_nli_cli():
         if 'Epoch 5	Loss:' in line:
             assert 'Loss: 1.0984' in line
         if 'Epoch 6	Loss:' in line:
-            assert 'Loss: 1.0985' in line
+            assert 'Loss: 1.0983' in line
         if 'Epoch 7 Loss:' in line:
-            assert 'Loss: 1.0968' in line
+            assert 'Loss: 1.0964' in line
         if 'Epoch 8 Loss:' in line:
-            assert 'Loss: 1.0765' in line
+            assert 'Loss: 1.0884' in line
         if 'Epoch 9 Loss:' in line:
-            assert 'Loss: 1.0765' in line
+            assert 'Loss: 1.0673' in line
         if 'Epoch 10    Loss:' in line:
-            assert 'Loss: 1.0451' in line
+            assert 'Loss: 1.0256' in line
 
         if 'Epoch 30    Loss:' in line:
-            assert 'Loss: 0.2969' in line
+            assert 'Loss: 0.3561' in line
 
         if 'Epoch 40    Loss:' in line:
-            assert 'Loss: 0.0658' in line
+            assert 'Loss: 0.1298' in line
 
         if 'Epoch 50    Loss:' in line:
-            assert 'Loss: 0.0079' in line
+            assert 'Loss: 0.0146' in line
 
 if __name__ == '__main__':
     pytest.main([__file__])
