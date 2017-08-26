4bef2b5c8aa8c7752044c165c29219847856d35e
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-08-26 22:03:51 +0200

    update

--- tests/inferbeddings/test_nli_cli.py
+++ tests/inferbeddings/test_nli_cli.py
@@ -9,29 +9,9 @@ sys.setrecursionlimit(65535)
 
 @pytest.mark.light
 def test_nli_cli():
-    # Checking if results are still nice
-    cmd = ['./bin/nli-cli.py',
-           '-f',
-           '-n',
-           '-m', 'ff-damp',
-           '--batch-size', '32',
-           '--representation-size', '200',
-           '--optimizer', 'adagrad',
-           '--learning-rate', '0.05',
-           '--restore', './models/nli/damp_v1.ckpt',
-           '--use-masking',
-           '--nb-epochs', '0']
-
-    # p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # out, err = p.communicate()
-
-    # print(err)
-    # print(err.split()[-1][:-1])
-
-    cmd_str = './bin/nli-cli.py -f -n -m ff-dam --batch-size 32 --dropout-keep-prob 0.8 ' \
-              '--representation-size 200 --optimizer adam --learning-rate 0.001 -c 100 ' \
-              '-i normal --nb-epochs 50 -t data/snli/tiny/tiny.jsonl.gz -v data/snli/tiny/tiny.jsonl.gz ' \
-              '-T data/snli/tiny/tiny.jsonl.gz'
+    cmd_str = './bin/nli-cli.py -f -n -m ff-dam --batch-size 32 --dropout-keep-prob 0.8 --representation-size 200 ' \
+              '--optimizer adam --learning-rate 0.001 -c 100 -i normal --nb-epochs 50 --has-bos -t ' \
+              'data/snli/tiny/tiny.jsonl.gz -v data/snli/tiny/tiny.jsonl.gz -T data/snli/tiny/tiny.jsonl.gz'
     cmd = cmd_str.split()
 
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -44,30 +24,18 @@ def test_nli_cli():
     for line in lines:
         print(line)
 
-        if 'Epoch 1	Loss:' in line:
-            assert 'Loss: 1.0988' in line
-        if 'Epoch 2	Loss:' in line:
-            assert 'Loss: 1.0986' in line
-        if 'Epoch 3	Loss:' in line:
-            assert 'Loss: 1.0986' in line
-        if 'Epoch 4	Loss:' in line:
-            assert 'Loss: 1.0986' in line
-        if 'Epoch 5	Loss:' in line:
-            assert 'Loss: 1.0983' in line
-        if 'Epoch 6	Loss:' in line:
-            assert 'Loss: 1.0984' in line
-        if 'Epoch 7 Loss:' in line:
-            assert 'Loss: 1.0941' in line
-        if 'Epoch 8 Loss:' in line:
-            assert 'Loss: 1.0876' in line
-        if 'Epoch 9 Loss:' in line:
-            assert 'Loss: 1.0632' in line
-        if 'Epoch 10    Loss:' in line:
-            assert 'Loss: 1.0202' in line
-        if 'Epoch 30    Loss:' in line:
-            assert 'Loss: 0.1977' in line
-        if 'Epoch 40    Loss:' in line:
-            assert 'Loss: 0.0087' in line
+        if 'Epoch 1/1' in line:
+            assert '0.0343' in line
+        if 'Epoch 10/1' in line:
+            assert '0.0316' in line
+        if 'Epoch 20/1' in line:
+            assert '0.0145' in line
+        if 'Epoch 30/1' in line:
+            assert '0.0105' in line
+        if 'Epoch 40/1' in line:
+            assert '0.0179' in line
+        if 'Epoch 50/1' in line:
+            assert '0.0008' in line
 
 if __name__ == '__main__':
     pytest.main([__file__])
