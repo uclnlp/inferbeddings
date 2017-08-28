d6666002d50591a8ce5168117280acdd89ef661c
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-08-28 19:03:08 +0200

    small corrective update to the model

--- inferbeddings/nli/dam.py
+++ inferbeddings/nli/dam.py
@@ -179,7 +179,7 @@ class FeedForwardDAM(BaseDecomposableAttentionModel):
         with tf.variable_scope('transform_embeddings', reuse=reuse) as _:
             projection = tf.contrib.layers.fully_connected(inputs=sequence, num_outputs=self.representation_size,
                                                            weights_initializer=tf.random_normal_initializer(0.0, 0.01),
-                                                           activation_fn=None)
+                                                           biases_initializer=None, activation_fn=None)
         return projection
 
     def _transform_attend(self, sequence, reuse=False):
