39f1f4f8389c8a6aabcab23eefec7262d4e983c4
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-09-02 20:32:23 +0200

    testing model loading

--- tests/inferbeddings/test_nli_cli.py
+++ tests/inferbeddings/test_nli_cli.py
@@ -43,5 +43,27 @@ def test_nli_cli():
         if 'Epoch 100/1' in line:
             assert '0.0008' in line
 
+
+@pytest.mark.light
+def test_nli_cli_restore():
+    cmd_str = './bin/nli-cli.py -f -n -m ff-dam --batch-size 32 --dropout-keep-prob 0.8 ' \
+              '--representation-size 200 --optimizer adam --learning-rate 0.0 -c 100 -i normal ' \
+              '--nb-epochs 1 --has-bos -t data/snli/tiny/tiny.jsonl.gz -v data/snli/tiny/tiny.jsonl.gz ' \
+              '-T data/snli/tiny/tiny.jsonl.gz --restore tests/models/snly_tiny_ffdam/dam_1 --report 1'
+    cmd = cmd_str.split()
+
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+
+    sys.stdout = sys.stderr
+
+    lines = err.decode("utf-8").split("\n")
+
+    for line in lines:
+        print(line)
+
+        if 'Dev Acc' in line:
+            assert 'Dev Acc: 100.00	Test Acc: 100.00' in line
+
 if __name__ == '__main__':
     pytest.main([__file__])
