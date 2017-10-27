b2e25ca862a796c852346cf9a8b5f71a80e52608
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-10-27 16:17:14 +0200

    update

--- data/snli/generated/generate.sh
+++ data/snli/generated/generate.sh
@@ -5,10 +5,17 @@
 ./tools/contradictions-cli.py ../snli_1.0_dev.jsonl.gz -i > snli_1.0_contradictions_inverse_dev.json
 ./tools/contradictions-cli.py ../snli_1.0_test.jsonl.gz -i > snli_1.0_contradictions_inverse_test.json
 
+zcat ../snli_1.0_dev.jsonl.gz > snli_1.0_all_dev.json
+zcat ../snli_1.0_test.jsonl.gz > snli_1.0_all_test.json
+
 ./tools/contradictions-cli.py ../snli_1.0_dev.jsonl.gz -r > snli_1.0_all_contradictions_dev.json
 ./tools/contradictions-cli.py ../snli_1.0_test.jsonl.gz -r > snli_1.0_all_contradictions_test.json
 ./tools/contradictions-cli.py ../snli_1.0_dev.jsonl.gz -i -r > snli_1.0_all_contradictions_inverse_dev.json
 ./tools/contradictions-cli.py ../snli_1.0_test.jsonl.gz -i -r > snli_1.0_all_contradictions_inverse_test.json
 
+./tools/neutrals-cli.py ../snli_1.0_dev.jsonl.gz -r > snli_1.0_all_neutrals_dev.json
+./tools/neutrals-cli.py ../snli_1.0_test.jsonl.gz -r > snli_1.0_all_neutrals_test.json
+./tools/neutrals-cli.py ../snli_1.0_dev.jsonl.gz -i -r > snli_1.0_all_neutrals_inverse_dev.json
+./tools/neutrals-cli.py ../snli_1.0_test.jsonl.gz -i -r > snli_1.0_all_neutrals_inverse_test.json
 
 md5sum *.json
