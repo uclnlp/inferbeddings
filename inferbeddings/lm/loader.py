37ca809a7deaa3c3f87d0284143be4eb1616255b
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2017-11-08 20:23:54 +0000

    update

--- inferbeddings/lm/loader.py
+++ inferbeddings/lm/loader.py
@@ -6,6 +6,8 @@ import json
 import numpy as np
 import nltk
 
+from inferbeddings.nli.util import pad_sequences
+
 import logging
 
 logger = logging.getLogger(__name__)
@@ -13,8 +15,9 @@ logger = logging.getLogger(__name__)
 
 class SNLILoader:
     def __init__(self,
-                 path='data/snli/snli_1.0_train.jsonl.gz',
-                 token_to_index=None):
+                 path='data/snli/snli_1.0_test.jsonl.gz',
+                 token_to_index=None,
+                 bos_idx=1, eos_idx=2, unk_idx=3):
 
         assert token_to_index is not None
         assert path is not None
@@ -34,10 +37,13 @@ class SNLILoader:
                 if gl in {'entailment', 'neutral', 'contradiction'}:
                     self.sentences += [s1, s2]
 
-        self.tensor = np.array(list(map(self.token_to_index.get, self.sentences)))
+        self.sentence_idxs = []
+        for sentence in self.sentences:
+            s_idxs = [self.token_to_index.get(word, unk_idx) for word in sentence]
+            self.sentence_idxs += [s_idxs]
 
-
-
+        self.tensor = pad_sequences(self.sentence_idxs)
+        
 
     @staticmethod
     def extract_sentences(obj):
@@ -50,7 +56,6 @@ class SNLILoader:
         sentence2_parse_tokens = sentence2_tree.leaves()
 
         gold_label = obj['gold_label']
-
         return sentence1_parse_tokens, sentence2_parse_tokens, gold_label
 
 
@@ -60,6 +65,11 @@ if __name__ == '__main__':
     path = 'models/snli/dam_1/dam_1_index_to_token.p'
     with open(path, 'rb') as f:
         index_to_token = pickle.load(f)
-    token_to_index = {token: index for index, token in index_to_token.items()}
 
+    token_to_index = {token: index for index, token in index_to_token.items()}
     loader = SNLILoader(token_to_index=token_to_index)
+
+    print(loader.tensor.shape)
+    print(loader.tensor)
+
+    print(np.nonzero(loader.tensor[:, -1]))
