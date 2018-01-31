cbb979eb91dc13ca3efeab6956e9859991874e8a
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2018-01-31 14:29:14 +0000

    update

--- data/snli/acl/v1/go.sh
+++ data/snli/acl/v1/go.sh
@@ -1,1 +1,1 @@
-./tools/sample.py -p ../../snli_1.0_test.jsonl.gz -f 0.01 | ./tools/invert.py | jq . | tr -d "\n" | sed -e ''s/"}"/"}\n"/g'' | jq . > start.json
+./tools/sample.py -p ../../snli_1.0_test.jsonl.gz -f 0.01 | ./tools/invert.py | jq . | tr -d "\n" | sed -e ''s/"}"/"}\n"/g'' | jq . > v1.1_start.json
