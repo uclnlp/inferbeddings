f63a1913115f8e8f217ed6bf890e65c0ea681431
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2018-01-31 20:52:00 +0000

    update

--- data/snli/acl/v1/tools/genadv.py
+++ data/snli/acl/v1/tools/genadv.py
@@ -31,7 +31,7 @@ def persist(path):
     def decorator(fun):
         cache = {}
         if os.path.isfile(path):
-            with open(path, 'r') as f:
+            with open(path, 'rb') as f:
                 cache = pickle.load(f)
 
         with open(path, 'wb') as f:
