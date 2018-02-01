bc6f6dde5f1cd5d61fd07b259b87d9616b07e791
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2018-02-01 18:11:10 +0000

    update

--- data/snli/acl/v1/tools/genadv.py
+++ data/snli/acl/v1/tools/genadv.py
@@ -115,16 +115,16 @@ def main(argv):
     else:
         sample_obj_lst = obj_lst
 
-    obj_to_c_loss = dict()
+    obj_c_loss_pairs = []
 
     for obj in sample_obj_lst:
         s1, s2 = obj['sentence1'], obj['sentence2']
         c_loss_value_dam = contradiction_loss_dam(s1, s2)
         c_loss_value_esim = contradiction_loss_esim(s1, s2)
 
-        obj_to_c_loss[obj] = c_loss_value_dam
+        obj_c_loss_pairs += [obj, c_loss_value_dam]
 
-    sorted_objs = [x for (x, _) in sorted(obj_to_c_loss.items(),
+    sorted_objs = [x for (x, _) in sorted(obj_c_loss_pairs,
                                           key=operator.itemgetter(1),
                                           reverse=True)]
 
