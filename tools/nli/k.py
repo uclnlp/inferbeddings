e6f3196aec27bdadd96f611698510514aac14a82
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2018-02-07 15:14:49 +0000

    update

--- tools/nli/k.py
+++ tools/nli/k.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
 import sys
 import glob
 
 # $ ls models/snli/dam_1/acl/ | grep batch_dsearch_reg_v | python3 k.py | grep -v mkdir  | parallel -j 8 --delay 6
 
+out_str = ''
 
 for s in sys.stdin:
     s = s.strip()
@@ -15,22 +20,29 @@ for s in sys.stdin:
         restore_path = tmp[0].split('_0.ind')[0]
 
         output_path = 'out_nli/k{}/'.format(p)
-        print('mkdir {}'.format(output_path))
+        out_str += """
+mkdir {}
+""".format(output_path)
 
         template = ''
         for (prefix, suffix) in [('train', ''), ('dev', 'd'), ('test', 't')]:
             template += """
+
 python3 ./bin/nli-debug-cli.py --has-bos --has-unk --batch-size 128 --restore {}_0 -d data/snli/*{}* 2>&1 | tail -n 20 > {}/1{}.log
 python3 ./bin/nli-debug-cli.py --has-bos --has-unk --batch-size 128 --restore {}_3 -d data/snli/*{}* 2>&1 | tail -n 20 > {}/2{}.log
 python3 ./bin/nli-debug-cli.py --has-bos --has-unk --batch-size 128 --restore {}_6 -d data/snli/*{}* 2>&1 | tail -n 20 > {}/3{}.log
 python3 ./bin/nli-debug-cli.py --has-bos --has-unk --batch-size 128 --restore {}_9 -d data/snli/*{}* 2>&1 | tail -n 20 > {}/4{}.log
 python3 ./bin/nli-debug-cli.py --has-bos --has-unk --batch-size 128 --restore {}_12 -d data/snli/*{}* 2>&1 | tail -n 20 > {}/5{}.log
 python3 ./bin/nli-debug-cli.py --has-bos --has-unk --batch-size 128 --restore {}_15 -d data/snli/*{}* 2>&1 | tail -n 20 > {}/6{}.log
+
 """.format(*(['{}', prefix, '{}', suffix] * 6))
 
-        print(template.format(*([restore_path, output_path] * (6 * 3))))
+        out_str += template.format(*([restore_path, output_path] * (6 * 3)))
 
-        print('mkdir {}/v1/'.format(output_path))
+        out_str += """
+mkdir {}/v1/
+""".format(output_path)
+
         temp = ''
         for size in ['100', '500', '1000', '2000', '3000', '4000', '5000', 'full']:
             for model in ['dam_', 'esim_', '']:
@@ -43,10 +55,15 @@ python3 ./bin/nli-debug-cli.py --has-bos --has-unk --batch-size 128 --restore {}
 python3 ./bin/nli-debug-cli.py --has-bos --has-unk --batch-size 128 --restore {}_15 -d data/snli/acl/v1/genadv/snli_genadv_{}{}_test.jsonl.gz 2>&1 | tail -n 20 > {}/v1/6_{}.log
 """.format(*(['{}', model, size, '{}', model + size] * 6))
 
-        print(temp.format(*([restore_path, output_path] * (6 * 8 * 3))))
+        out_str += temp.format(*([restore_path, output_path] * (6 * 8 * 3)))
 
-        print('mkdir {}/v1.1/'.format(output_path))
-        print('mkdir {}/v1.2/'.format(output_path))
+        out_str += """
+mkdir {}/v1.1/
+""".format(output_path)
+
+        out_str += """
+mkdir {}/v1.2/
+""".format(output_path)
 
         t = """
 python3 ./bin/nli-debug-cli.py --has-bos --has-unk --batch-size 128 --restore {}_0 -d data/snli/acl/v1/v1.1_edited.jsonl.gz 2>&1 | tail -n 20 > {}/v1.1/1.log
@@ -64,4 +81,18 @@ python3 ./bin/nli-debug-cli.py --has-bos --has-unk --batch-size 128 --restore {}
 python3 ./bin/nli-debug-cli.py --has-bos --has-unk --batch-size 128 --restore {}_15 -d data/snli/acl/v1/v1.2_edited.jsonl.gz 2>&1 | tail -n 20 > {}/v1.2/6.log
         """.format(*(['{}', '{}'] * 12))
 
-        print(t.format(*([restore_path, output_path] * (6 * 2))))
+        out_str += t.format(*([restore_path, output_path] * (6 * 2)))
+
+out_lst = out_str.split("\n")
+
+for e in out_lst:
+    e_split = e.split(" ")
+    if len(e_split) > 0:
+        path = e_split[-1]
+
+        if os.path.isfile(path) and 'log' in path:
+            with open(path, 'r') as f:
+                data = f.read()
+
+            if '(True) AND NOT(S1' not in data:
+                print(e)
