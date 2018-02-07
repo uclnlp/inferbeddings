870ba51001ba18a066854bb9892eb6410eb6e44c
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2018-02-07 11:46:12 +0000

    generator, first stub with tests

--- inferbeddings/nli/generate/generator.py
+++ inferbeddings/nli/generate/generator.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import nltk
 import numpy as np
 
@@ -8,12 +9,14 @@ from inferbeddings.nli.generate.parser import Parser
 
 class Generator:
     def __init__(self, token_to_index, corenlp_url='http://127.0.0.1:9000',
-                 is_remove=True, is_merge=True, nb_corruptions=32, nb_words=1024,
+                 is_remove=True, is_merge=True,
+                 nb_corruptions=32, nb_words=1024,
                  bos_idx=1, eos_idx=2, unk_idx=3,
                  seed=0):
 
         self.token_to_index = token_to_index
         self.index_to_token = {k: v for v, k in self.token_to_index.items()}
+        self.unk_token = self.index_to_token[unk_idx]
 
         self.corenlp_url = corenlp_url
         self.is_remove = is_remove
@@ -31,7 +34,13 @@ class Generator:
         self.tokenizer = nltk.tokenize.TreebankWordTokenizer()
 
     def generate(self, sentence1, sentence2):
-        pass
+        # If the input is arrays of indices, turn each array into a string first
+        sentence1_str, sentence2_str = sentence1, sentence2
+        if isinstance(sentence1, list):
+            sentence1_str = self._to_string(sentence1)
+            sentence2_str = self._to_string(sentence2)
+
+        return
 
     # TODO: write test cases
     def remove(self, sentence1, sentence2):
@@ -45,12 +54,11 @@ class Generator:
             sentence2_idxs = [self.token_to_index.get(tkn, self.unk_idx) for tkn in sentence2_tkns]
         else:
             assert isinstance(sentence1, list) and isinstance(sentence2, list)
-
             sentence1_idxs = sentence1
             sentence2_idxs = sentence2
 
-        corruptions1 = np.repeat(a=[sentence1], repeats=self.nb_corruptions, axis=0)
-        corruptions2 = np.repeat(a=[sentence2], repeats=self.nb_corruptions, axis=0)
+        corruptions1 = np.repeat(a=[sentence1_idxs], repeats=self.nb_corruptions, axis=0)
+        corruptions2 = np.repeat(a=[sentence2_idxs], repeats=self.nb_corruptions, axis=0)
 
         sentence1_len, sentence2_len = len(sentence1_idxs), len(sentence2_idxs)
 
@@ -64,14 +72,18 @@ class Generator:
                 where_to_corrupt2 = self.rs.randint(low=1, high=sentence2_len - 1)
                 corruptions2[idx, where_to_corrupt2] = new_word
 
-        res1, res2 = corruptions1, corruptions2
+        res1, res2 = corruptions1.tolist(), corruptions2.tolist()
 
         # If the input is strings, the outputs needs to be strings as well.
         if isinstance(sentence1, str):
-            res1 = ' '.join([self.index_to_token[idx] for idx in corruptions1])
-            res2 = ' '.join([self.index_to_token[idx] for idx in corruptions2])
+            new_res1 = [self._to_string(e) for e in res1]
+            new_res2 = [self._to_string(e) for e in res2]
+            res1, res2 = new_res1, new_res2
 
         return res1, res2
+
+    def _to_string(self, sentence_idxs):
+        return ' '.join([self.index_to_token.get(idx, self.unk_token) for idx in sentence_idxs])
 
     def _tokenize(self, sentence):
         return self.tokenizer.tokenize(sentence)
--- tests/inferbeddings/nli/generate/test_generator.py
+++ tests/inferbeddings/nli/generate/test_generator.py
@@ -2,9 +2,48 @@
 
 import pytest
 
+import pickle
+import nltk
+
+from inferbeddings.nli.generate.generator import Generator
+
 
 def test_generator():
-    pass
+    vocabulary_path = 'models/snli/dam_1/dam_1_index_to_token.p'
+
+    with open(vocabulary_path, 'rb') as f:
+        index_to_token = pickle.load(f)
+
+    index_to_token.update({
+        0: '<PAD>',
+        1: '<BOS>',
+        2: '<UNK>'
+    })
+
+    token_to_index = {v: k for k, v in index_to_token.items()}
+
+    nb_corruptions = 32
+    g = Generator(token_to_index=token_to_index, nb_corruptions=nb_corruptions)
+
+    sentence1 = 'The girl runs happily on the grass .'
+    sentence2 = 'The girl is happy .'
+
+    corr1, corr2 = g.remove(sentence1=sentence1, sentence2=sentence2)
+    assert len(corr1) == len(corr2) == nb_corruptions
+
+    tokenizer = nltk.tokenize.TreebankWordTokenizer()
+
+    for e in corr1:
+        e_tkns = tokenizer.tokenize(e)
+        s_tkns = tokenizer.tokenize(sentence1)
+        assert len(e_tkns) == len(s_tkns)
+
+    for e in corr2:
+        e_tkns = tokenizer.tokenize(e)
+        s_tkns = tokenizer.tokenize(sentence2)
+        assert len(e_tkns) == len(s_tkns)
+
 
 if __name__ == '__main__':
     pytest.main([__file__])
+    # test_generator()
