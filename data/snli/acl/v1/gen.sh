5fb9b6260f772e118ec4b85f4db1bdaed8dfccac
Pasquale Minervini <pasquale.minervini@insight-centre.org>
2018-02-13 19:10:17 +0000

    update

--- data/snli/acl/v1/gen.sh
+++ data/snli/acl/v1/gen.sh
@@ -16,6 +16,15 @@
 ./tools/genadv_esim.py -p ../../snli_1.0_dev.jsonl.gz -n 5000 > genadv/snli_genadv_esim_5000_dev.jsonl
 ./tools/genadv_esim.py -p ../../snli_1.0_dev.jsonl.gz > genadv/snli_genadv_esim_full_dev.jsonl
 
+./tools/genadv_cbilstm.py -p ../../snli_1.0_dev.jsonl.gz -n 100 > genadv/snli_genadv_cbilstm_100_dev.jsonl
+./tools/genadv_cbilstm.py -p ../../snli_1.0_dev.jsonl.gz -n 500 > genadv/snli_genadv_cbilstm_500_dev.jsonl
+./tools/genadv_cbilstm.py -p ../../snli_1.0_dev.jsonl.gz -n 1000 > genadv/snli_genadv_cbilstm_1000_dev.jsonl
+./tools/genadv_cbilstm.py -p ../../snli_1.0_dev.jsonl.gz -n 2000 > genadv/snli_genadv_cbilstm_2000_dev.jsonl
+./tools/genadv_cbilstm.py -p ../../snli_1.0_dev.jsonl.gz -n 3000 > genadv/snli_genadv_cbilstm_3000_dev.jsonl
+./tools/genadv_cbilstm.py -p ../../snli_1.0_dev.jsonl.gz -n 4000 > genadv/snli_genadv_cbilstm_4000_dev.jsonl
+./tools/genadv_cbilstm.py -p ../../snli_1.0_dev.jsonl.gz -n 5000 > genadv/snli_genadv_cbilstm_5000_dev.jsonl
+./tools/genadv_cbilstm.py -p ../../snli_1.0_dev.jsonl.gz > genadv/snli_genadv_cbilstm_full_dev.jsonl
+
 ./tools/genadv.py -p ../../snli_1.0_dev.jsonl.gz -n 100 > genadv/snli_genadv_100_dev.jsonl
 ./tools/genadv.py -p ../../snli_1.0_dev.jsonl.gz -n 500 > genadv/snli_genadv_500_dev.jsonl
 ./tools/genadv.py -p ../../snli_1.0_dev.jsonl.gz -n 1000 > genadv/snli_genadv_1000_dev.jsonl
@@ -44,6 +53,15 @@
 ./tools/genadv_esim.py -p ../../snli_1.0_test.jsonl.gz -n 4000 > genadv/snli_genadv_esim_4000_test.jsonl
 ./tools/genadv_esim.py -p ../../snli_1.0_test.jsonl.gz -n 5000 > genadv/snli_genadv_esim_5000_test.jsonl
 ./tools/genadv_esim.py -p ../../snli_1.0_test.jsonl.gz > genadv/snli_genadv_esim_full_test.jsonl
+
+./tools/genadv_cbilstm.py -p ../../snli_1.0_test.jsonl.gz -n 100 > genadv/snli_genadv_cbilstm_100_test.jsonl
+./tools/genadv_cbilstm.py -p ../../snli_1.0_test.jsonl.gz -n 500 > genadv/snli_genadv_cbilstm_500_test.jsonl
+./tools/genadv_cbilstm.py -p ../../snli_1.0_test.jsonl.gz -n 1000 > genadv/snli_genadv_cbilstm_1000_test.jsonl
+./tools/genadv_cbilstm.py -p ../../snli_1.0_test.jsonl.gz -n 2000 > genadv/snli_genadv_cbilstm_2000_test.jsonl
+./tools/genadv_cbilstm.py -p ../../snli_1.0_test.jsonl.gz -n 3000 > genadv/snli_genadv_cbilstm_3000_test.jsonl
+./tools/genadv_cbilstm.py -p ../../snli_1.0_test.jsonl.gz -n 4000 > genadv/snli_genadv_cbilstm_4000_test.jsonl
+./tools/genadv_cbilstm.py -p ../../snli_1.0_test.jsonl.gz -n 5000 > genadv/snli_genadv_cbilstm_5000_test.jsonl
+./tools/genadv_cbilstm.py -p ../../snli_1.0_test.jsonl.gz > genadv/snli_genadv_cbilstm_full_test.jsonl
 
 ./tools/genadv.py -p ../../snli_1.0_test.jsonl.gz -n 100 > genadv/snli_genadv_100_test.jsonl
 ./tools/genadv.py -p ../../snli_1.0_test.jsonl.gz -n 500 > genadv/snli_genadv_500_test.jsonl
